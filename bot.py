1408b202fd16d7ce0fe46cd487aa6a50c269ca91
Dayan <163378331+dayannoob@users.noreply.github.com>
2025-11-14 12:03:53 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,19 +2,25 @@ import os
 import discord
 from discord.ext import commands
 
+# فعال کردن Intents
 intents = discord.Intents.default()
 intents.message_content = True
 
-bot = commands.Bot(command_prefix=!, intents=intents)
+# ساخت ربات با prefix "!"
+bot = commands.Bot(command_prefix="!", intents=intents)
 
+# وقتی ربات روشن شد
 @bot.event
-async def on_ready()
-    print(f{bot.user} is online!)
+async def on_ready():
+    print(f"{bot.user} is online!")
+    # وضعیت ربات را آنلاین و با Activity مشخص می‌کنیم
+    await bot.change_presence(activity=discord.Game(name="!ping"))
 
+# دستور ساده ping
 @bot.command()
-async def ping(ctx)
+async def ping(ctx):
     await ctx.send("Pong! 🏓")
 
-TOKEN = os.getenv(DISCORD_TOKEN)
+# اجرای ربات با توکن از Environment Variable
+TOKEN = os.getenv("DISCORD_TOKEN")
 bot.run(TOKEN)
-
